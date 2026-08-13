291c0023d2646453ed71c262e73b6c94a5da3070
christinabrooman <cbrooma@wgu.edu>
2026-08-13 14:50:20 -0700

    Modify main.py for basic cleaning section of pipeline

--- main.py
+++ main.py
@@ -50,10 +50,21 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(".", "src", "basic_cleaning"),
+                "main",
+                env_manager="conda",
+                parameters={
+                    "input_artifact": "sample.csv:latest",
+                    "output_artifact": "clean_sample.csv",
+                    "output_type": "cleaned_data",
+                    "output_description": "Cleaned version of Airbnb dataset",
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                },
+            )
+
+
 
         if "data_check" in active_steps:
             ##################
