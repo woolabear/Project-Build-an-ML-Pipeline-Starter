fd4067284b066e66f4108952c2d407856a8dd790
christinabrooman <cbrooma@wgu.edu>
2026-08-13 16:25:19 -0700

    Modify main.py and test_data.py for data_check

--- main.py
+++ main.py
@@ -64,13 +64,19 @@ def go(config: DictConfig):
                 },
             )
 
-
-
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                os.path.join(".", "src", "data_check"),
+                "main",
+                env_manager="conda",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]
+                },
+            )
 
         if "data_split" in active_steps:
             ##################
--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -84,6 +84,10 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert np.isfinite(kl_div) and kl_div < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+# my row count test
+def test_row_count(data):
+    assert 15000 < data.shape[0] < 1000000
+
+# my price range test
+def test_price_range(data, min_price, max_price):
+    assert data['price'].between(min_price, max_price).all()
